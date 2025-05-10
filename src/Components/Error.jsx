71f57ebb93c8e34d8f39ad22bab266013f3d0b05
Lamiya <akabane755@gmail.com>
2025-05-10 23:36:21 +0600

    error page fixed

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
     return (
         <div>
            <Navb></Navb>
-           <div className='flex flex-col items-center space-y-4 '>
+           <div className='flex flex-col items-center space-y-4 mt-10'>
           <p className='text-6xl'>404 Error</p>
        <Link to='/'> <button className='btn btn-primary'>Go to home</button></Link>  
           </div>
